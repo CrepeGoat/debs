2d16fc69151c22b5e787baca0c4659a6757db67d
CrepeGoat <awqatty.b@gmail.com>
2024-01-26 20:27:25 -0700

    wip

---
+++ src/density-estimator.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const bspline = @import("b-splines.zig");
+
+pub fn PdfEstimateBuilder(comptime T: type) type {
+    _ = T; // autofix
+    return struct {
+
+        pub fn new(samples: )
+    };
+}
